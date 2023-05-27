51e7e61ae65276289c18be6156a8e5632a7aff06
Claire Y <61437734+ClaireYurev@users.noreply.github.com>
2023-05-27 14:34:07 -0700

    Update question_5.py

--- diagnostic/question_5.py
+++ diagnostic/question_5.py
@@ -6,12 +6,15 @@ def main():
     x = 10
     y = 10
     draw_car(canvas, x, y)
+    # ^ Added arguments canvas, x, and y into the draw_car() call
 
     x = 100
     y = 100
     draw_car(canvas, x, y)
+    # ^ Added arguments canvas, x, and y into the draw_car() call again
 
 def draw_car(given_canvas, given_x, given_y):
+    # ^ Added parameters given_canvas, given_x, and given_y into definition
     # draws a car at the location x, y
     # you can assume that math offsets for the rectangles are correct
     given_canvas.create_rectangle(given_x, given_y, given_x + 50, given_y + 20)
