fd98e7e61bc555da55b578b43ccf05d4940e0905
Claire Y <61437734+ClaireYurev@users.noreply.github.com>
2023-06-02 10:09:21 -0700

    Update Baby Snake - One Level.py

--- Week 6/Baby Snake - One Level.py
+++ Week 6/Baby Snake - One Level.py
@@ -13,20 +13,12 @@ TARGET_COLOR = "pink"
 
 def main():
     
-    # How many levels to play:
-    game_levels = 10
+    # Create a canvas
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
     
-    while game_levels > 0:
+    # Set up and launch the "snake" at point of "ORIGIN", e.g. (0, 0)
+    create_and_run_snake(canvas)
         
-        # Create a canvas
-        canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
-    
-        # Set up and launch the "snake" at point of "ORIGIN", e.g. (0, 0)
-        create_and_run_snake(canvas)
-        
-        # Decrement the game_levels
-        game_levels -= 1
-    
     # Final message, after game is finished (either Won, or Lost)
     print('Thank you for playing Baby Snake :)\n')
     
@@ -176,14 +168,32 @@ def random_location():
     # The locations are done in increments of 20 pixels, based on the SIZE constant
     locations = []
     
-    # we choose to not include a possibility of the Target being placed at 0,0
-    # Also, we assume the canvas is SQUARE - otherwise we'll purposely throw an error:
+
+    # We choose to assume that the canvas is SQUARE. I.e. the HEIGHT equals LENGTH.
+    # This is why we define possible_locations variable inside the "if" statement.
+    # This is done on purpose, so that if the canvas is NOT square, we'll get an error, and the game won't run.
+    
     if CANVAS_HEIGHT == CANVAS_WIDTH:
+        
         possible_locations = CANVAS_HEIGHT / SIZE - 1
+        """
+            Above, you'll notice the "- 1". This subtraction of 1 is done on purpose:
+            We choose to not include the possibility of the Target being placed at 0,0.
+            To illustrate this, uncomment line 196 (the 'print(locations)' line).
+            You will see the following:
+            [20.0, 40.0, 60.0, 80.0, 100.0, 120.0, 140.0, 160.0, 180.0, 200.0, 220.0, 240.0, 260.0, 280.0]
+            In other words, there is no location at  "0.0", in the above list.
+            Please try to run the same, but without the "- 1", i.e. "possible_locations = CANVAS_HEIGHT / SIZE"
+            In that case, without the "- 1", you would see the following:
+            [0.0, 20.0, 40.0, 60.0, 80.0, 100.0, 120.0, 140.0, 160.0, 180.0, 200.0, 220.0, 240.0, 260.0, 280.0]
+            Control this as you wish :)
+        """
     
     while possible_locations > 0:
         locations.append(CANVAS_HEIGHT - SIZE * possible_locations)
         possible_locations -= 1
+    
+    #print(locations)
     
     return random.choice(locations)
 
